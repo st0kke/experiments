acc3747a5a64d65f72117c91a7b3c0bd19f55594
st0kke <st0kke@users.noreply.githib.com>
2015-04-23 22:35:13 +0200

    started adding upload functionality

--- backbone/photoapp/public/app.js
+++ backbone/photoapp/public/app.js
@@ -2,6 +2,30 @@ _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
 };
 
+var AddPhotoView = Backbone.View.extend({
+  tagName: "form",
+  initialize: function(options) {
+    this.photos = options.photos; 
+  },
+  template: _.template($("#addPhotoView").html()),
+  events: {
+    "click button":"uploadFile"
+  },
+  render: function() {
+    this.el.innerHTML = this.template();
+    return this;
+  },
+  uploadFile: function(evt) {
+    evt.preventDefault();
+    var photo = new Photo({
+      file: $("imageUpload")[0].files[0],
+      caption: $("#imageCaption").val()
+    });
+    this.photos.create(photo, {wait: true});
+    this.el.reset();
+  }
+});
+
 var NavView = Backbone.View.extend({
   template: _.template($("#navView").html()),
   render: function() {
