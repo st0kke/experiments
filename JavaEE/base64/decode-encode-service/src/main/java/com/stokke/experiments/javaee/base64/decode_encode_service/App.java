bbc48042dca25466812331f6718be8f5abd23bfd
st0kke <st0kke@users.noreply.githib.com>
2016-04-27 22:53:49 +0200

    create skeleton

---
+++ JavaEE/base64/decode-encode-service/src/main/java/com/stokke/experiments/javaee/base64/decode_encode_service/App.java
@@ -0,0 +1,13 @@
+package com.stokke.experiments.javaee.base64.decode_encode_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
