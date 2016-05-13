24ee9e544b16b32dba059bab542b3840508c50a0
st0kke <st0kke@users.noreply.githib.com>
2016-05-13 22:58:37 +0200

    added decoding textareas

--- backbone/base64EncodeDecode/server.js
+++ backbone/base64EncodeDecode/server.js
@@ -1,57 +1,14 @@
 var express = require('express');
 var path = require('path');
-var bourne = require('bourne');
-
 var app = express();
-var posts = new bourne("simpleBlogPosts.json");
-var comments = new bourne("simpleBlogComments.json");
 
 app.configure(function() {
   app.use(express.json());
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-/*
-app.get("/posts", function (req, res) {
-  posts.find(function (results) {
-    res.json(results);
-  });
-});
-
-*/
-app.get("/posts", function (req, res) {
-     posts.find(function (abc, results) {
-       res.json(results);
-     });
-});
-
-app.get("/posts", function (req, res) {
-	res.json('{"result":' + posts + '}');
-});
-
-app.post("/posts", function (req, res) {
-  posts.insert(req.body, function (result) {
-    res.json(result);
-  });
-});
-
-app.get("/posts/:id/comments", function (req, res) {
-    comments.find({ postId: parseInt(req.params.id, 10) }, function (err, results) {
-        res.json(results);
-    });
-});
-
-app.post("/posts/:id/comments", function (req, res) {
-    comments.insert(req.body, function (err, result) {
-        res.json(result);
-    });
-});  
-
 app.get('/*', function(req, res) {
-  posts.find(function (err, results) {
-   res.render("index.ejs", {posts: JSON.stringify(results)} );
-  });
+	res.render("index.ejs");
 });
 
 app.listen(3000);
-
