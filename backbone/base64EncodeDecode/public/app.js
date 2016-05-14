73227aad52e2e282ab980f31d0d5cde43f7657bd
st0kke <st0kke@users.noreply.githib.com>
2016-05-14 23:04:25 +0200

    small update

--- backbone/base64EncodeDecode/public/app.js
+++ backbone/base64EncodeDecode/public/app.js
@@ -1,3 +1,17 @@
 _.templateSettings = {
      interpolate: /\{\{(.+?)\}\}/g
 };	
+
+var DecodeRouter = Backbone.Router.extend({
+	initialize: function(options) {
+	  this.main = options.main;
+	},
+	routes: {
+	  '':'index',
+          'decode': 'decode'
+	},
+	index: function() {
+	  var dv = new DecodeView({model: DecodeModel());
+	  this.main.html(dv.render().el); 
+	}
+});
