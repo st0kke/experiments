40ab317b42bd0f375b77f07e0c1a787bef1bd6ae
st0kke <st0kke@users.noreply.githib.com>
2016-05-15 22:57:13 +0200

    chnages allow rendering

--- backbone/base64EncodeDecode/public/app.js
+++ backbone/base64EncodeDecode/public/app.js
@@ -2,16 +2,38 @@ _.templateSettings = {
      interpolate: /\{\{(.+?)\}\}/g
 };	
 
+var DecodeView = Backbone.View.extend({
+  initalize: function() {
+    console.log("decode view setup");
+  },
+  model:DecodeModel,  
+  template: _.template($('#decodeView').html()),
+  render: function() {
+    var model = this.model.toJSON();
+    this.el.innerHTML = this.template(model);
+    return this;
+  }
+
+});
+
+var DecodeModel = Backbone.Model.extend({});
+
 var DecodeRouter = Backbone.Router.extend({
 	initialize: function(options) {
 	  this.main = options.main;
 	},
 	routes: {
 	  '':'index',
-          'decode': 'decode'
+          'decode': 'decodeString'
 	},
 	index: function() {
-	  var dv = new DecodeView({model: DecodeModel());
+	  var decodeModel = new DecodeModel({toBeDecoded: "enter string to be decoded"});	       
+	  var dv = new DecodeView({model: decodeModel});
 	  this.main.html(dv.render().el); 
+	},
+	decodeString: function() {
+          console.log('decoding.....');	
 	}
+
 });
+
