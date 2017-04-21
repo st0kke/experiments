bdec820d5dca5ff53d146c6f90bf3cf21433d1cb
st0kke <st0kke@users.noreply.githib.com>
2017-04-21 23:57:28 +0200

    completed changes

--- spring/spring4-auto-scanning/src/main/java/com/st0kke/experiments/spring/model/Employee.java
+++ spring/spring4-auto-scanning/src/main/java/com/st0kke/experiments/spring/model/Employee.java
@@ -1,5 +1,43 @@
 package com.st0kke.experiments.spring.model;
 
+import org.joda.time.LocalDate;
+
 public class Employee {
+
+    private int id;
+
+    private String name;
+
+    private LocalDate assessmentDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getAssessmentDate() {
+        return assessmentDate;
+    }
+
+    public void setAssessmentDate(LocalDate assessmentDate) {
+        this.assessmentDate = assessmentDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", assessmentDate="
+                + assessmentDate + "]";
+    }
 
 }
