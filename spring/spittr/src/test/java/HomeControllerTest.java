2df813ed3bfc8ab36cdc778e6beb3f6690277ff7
st0kke <st0kke@users.noreply.githib.com>
2017-05-08 23:27:47 +0200

    added some spittr functionality

---
+++ spring/spittr/src/test/java/HomeControllerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import spittr.web.HomeController;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+}
