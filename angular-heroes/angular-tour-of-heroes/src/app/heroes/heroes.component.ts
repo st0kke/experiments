cb4e6c35b61aa0965f26141ef8266ec267bb176a
st0kke <st0kke@users.noreply.githib.com>
2017-12-12 11:23:32 +0100

    added heroservice, and messageservice

--- angular-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
-
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,17 +11,21 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-  heroes = HEROES;
+  heroes:Hero[];
 
   onSelect(hero:Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { 
-    
+  constructor(private heroService:HeroService) { 
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
