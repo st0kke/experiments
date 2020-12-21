69d25d80226bc9aeac2ea4d1af997fe5e6769079
st0kke <st0kke@users.noreply.githib.com>
2020-12-21 13:58:45 +0100

    numbers #2

--- python/hex.py
+++ python/hex.py
@@ -1,18 +1,16 @@
 def hex_output(hex_num):
+    print(f'Converting 0x{hex_num} to decimal:')    
     result = 0
-    convs = {'a':10, 'b':11, 'c':12, 'd':13, 'e':14, 'f':15}
-
-
-    for index,h in enumerate(reversed(hex_num)):
-        if convs[h]:
-            h = convs[h]
-        val = (16**index * h)
-        print(f'(16^{index} * {h}) = {val}')
+    try:
+      for power,hex_char in enumerate(reversed(hex_num)):
+        val = (16 ** power) * int(hex_char, 16)
+        print(f'\t(16^{power} * {int(hex_char, 16)}) = {val}')
         result = result + val
-
-    print(f'0x{hex_num} converted to decimal is {result}')    
-
+    except ValueError as e:
+        print(f'\t{e}')
+    else:
+      print(f'\tResult is {result}')    
 
 
 hex_output('def')
-hex_output('f')
+hex_output('g')
---
+++ python/name.py
@@ -0,0 +1,9 @@
+def name_triangle():
+    output = ''
+    name = input('enter your name ')
+
+    for char in name:
+        output += char
+        print(output)
+
+name_triangle()
